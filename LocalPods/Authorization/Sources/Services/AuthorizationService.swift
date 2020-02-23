f26edfc2f2cca415e749af8d401f6233abaf5baf
Kurban Minkailov <>
2020-02-23 22:45:05 +0300

    Add protocol for AuthorizationService

--- LocalPods/Authorization/Sources/Services/AuthorizationService.swift
+++ LocalPods/Authorization/Sources/Services/AuthorizationService.swift
@@ -8,21 +8,31 @@
 import Foundation
 import KeychainAccess
 
-public class AuthorizationService {
+public protocol IAuthorizationService {
+    func set(pinCode: String, login: String, password: String, completion: (Result<Void, Error>) -> Void)
+    func verify(pinCode: String, completion: (Result<Void, Error>) -> Void)
+    func resetPinCode()
+}
+
+public class AuthorizationService: IAuthorizationService {
     
     private let pinCodeKey = "PinCodeKey"
+    private let loginKey = "LoginKey"
+    private let passwordKey = "PasswordKey"
     private let keychain = Keychain()
-    
-    func set(pinCode: String, completion: (Result<Void, Error>) -> Void) {
+
+    public func set(pinCode: String, login: String, password: String, completion: (Result<Void, Error>) -> Void) {
         do {
             try keychain.set(pinCode, key: pinCodeKey)
+            try keychain.set(pinCode, key: loginKey)
+            try keychain.set(pinCode, key: passwordKey)
             completion(Result.success(()))
         } catch let error {
             completion(Result.failure(error))
         }
     }
     
-    func verify(pinCode: String, completion: (Result<Void, Error>) -> Void) {
+    public func verify(pinCode: String, completion: (Result<Void, Error>) -> Void) {
         do {
             let savedPinCode = try keychain.get(pinCodeKey)
             
@@ -36,7 +46,7 @@ public class AuthorizationService {
         }
     }
     
-    func resetPinCode() {
+    public func resetPinCode() {
         do {
             try keychain.remove(pinCodeKey)
         } catch {
