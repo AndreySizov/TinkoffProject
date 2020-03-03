2d8b3f312bc0e46c1cffe93033baa1ef97a0eae7
Kurban Minkailov <>
2020-03-04 01:37:30 +0300

    Add storage manager

---
+++ TinkoffProject/Storage/Symbol.swift
@@ -0,0 +1,29 @@
+//
+//  Language.swift
+//  TinkoffProject
+//
+//  Created by pro on 02.03.2020.
+//  Copyright © 2020 Tinkoff. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct Symbol: Decodable {
+    var symbolId: String
+    var symbolDescription: String
+    var displaySymbol: String
+}
+ 
+extension Symbol: ManagedObjectConvertible {
+    typealias ManagedObject = ManagedSymbol
+    
+    func toManagedObject(in context: NSManagedObjectContext) -> ManagedSymbol {
+        let predicate = NSPredicate(format: "symbolId == %@", symbolId)
+        let managedSymbol = ManagedSymbol.findOrInsertObject(predicate: predicate, in: context)
+        managedSymbol.symbolId = symbolId
+        managedSymbol.symbolDescription = symbolDescription
+        managedSymbol.displaySymbol = displaySymbol
+        return managedSymbol
+    }
+}
